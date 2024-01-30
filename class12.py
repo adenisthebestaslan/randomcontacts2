0d5d812a207be478b69af3334dd94d5bd165e6e5
adenisthebestaslan <aden.a.oztas@gmail.com>
2024-01-29 18:02:37 -0600

    sadfhasdjfhjasdhfas

--- class12.py
+++ class12.py
@@ -1,1 +1,21 @@
-print('hello world!')
+input_list = [1, 2, 3, 4, 5]
+var1 = input_list[0]
+var2 = input_list[1]
+var3 = input_list[2]
+var4 = input_list[3]
+
+for i in range(len(input_list)):
+    print(i,i+1)
+
+
+
+
+# subnum = var1 + var2 + var3
+# varsub = subnum - var4
+
+
+# if varsub == 1:
+#     print('monotonic')
+# else:
+#     if varsub <= 0:
+#         print('monotonic')
